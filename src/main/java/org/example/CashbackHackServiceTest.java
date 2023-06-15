f826a9bba08b2ea67d39a50e1c08b80464c2d5aa
Diana Pribytkova <dianababayanarturovna777@gmail.com>
2023-06-15 18:49:41 +0300

    Junit4 Test2

--- src/main/java/org/example/CashbackHackServiceTest.java
+++ src/main/java/org/example/CashbackHackServiceTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertEquals;
 
 public class CashbackHackServiceTest {
    @Test
-   public void shouldCountBoundary(){
+   public void whenTheAmountEqualBoundary(){
        CashbackHackService cashback = new CashbackHackService();
        int amount = 1000;
 
@@ -15,4 +15,14 @@ public class CashbackHackServiceTest {
 
        assertEquals(expected, actual);
    }
+    @Test
+    public void whenTheAmountLessBoundary(){
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 585;
+
+        int actual = cashback.remain(amount);
+        int expected = 415;
+
+        assertEquals(expected, actual);
+    }
 }
