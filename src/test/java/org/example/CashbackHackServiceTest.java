14e89a7597eb885f7c0861729e27cf8743a65bc4
Diana Pribytkova <dianababayanarturovna777@gmail.com>
2023-06-19 15:21:26 +0300

    Edit test

--- src/test/java/org/example/CashbackHackServiceTest.java
+++ src/test/java/org/example/CashbackHackServiceTest.java
@@ -15,4 +15,14 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual, expected);
 }
+    @Test
+    public void whenTheAmountEqualBoundary() {
+        CashbackHackService cashback = new CashbackHackService();
+        int amount = 1000;
+
+        int actual = cashback.remain(amount);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
 }
